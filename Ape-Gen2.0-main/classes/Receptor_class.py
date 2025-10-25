33482e6d61a2a32cde7035274beb67e876e7a41b
jared-slone <51179852+jared-slone@users.noreply.github.com>
2025-10-25 11:32:11 -0500

    oops

--- Ape-Gen2.0-main/classes/Receptor_class.py
+++ Ape-Gen2.0-main/classes/Receptor_class.py
@@ -172,12 +172,12 @@ def model_receptor(allele_sequence, peptide_sequence, allotype, filestore, cv):
 	# model MHC with tfold
 	B2M = '' # TODO: Fix this
 	os.chdir('../../../tfold')
-	with open('MHC'+"".join(random.choices(string.ascii_letters + string.digits, k=10))+'.fasta','w') as f:
+	with open('MHC'+ran_key+'.fasta','w') as f:
 		f.write('>M\n'+allele_sequence+'\n')
 		f.write('>N\n'+B2M+'\n')
 		f.write('>P\n'+peptide_sequence+'\n')
 		# python projects/tfold_tcr/predict.py --json examples/pmhc_example.json --output examples/predictions/ --model_version pMHC
-	os.system('python3 projects/tfold_tcr/predict.py --fasta MHC'+"".join(random.choices(string.ascii_letters + string.digits, k=10))+'.fasta --output output/'+filestore+'MHC.pdb --model_version pMHC')
+	os.system('python3 projects/tfold_tcr/predict.py --fasta MHC'+ran_key+'.fasta --output output/'+filestore+'MHC.pdb --model_version pMHC')
 	os.chdir('../STEGG_controler')
 
 	ppdb = PandasPdb().read_pdb('../tfold/output/'+filestore+'MHC.pdb')
